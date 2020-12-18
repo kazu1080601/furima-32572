f273f1b8473867bff5e25f60a96eabebe4a6c8b1
kazu1080601 <kazu1080601@gmail.com>
2020-12-18 15:49:52 +0900

    出品のバリデーション設定修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,7 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :preparation
 
+  validates :image, :name, :text, presence: true
   validates :category_id, :condition_id, :feebearing_id, :area_id, :preparation_id, numericality: { other_than: 1 }
+  validates :price, presence: true
 end
