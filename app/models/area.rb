546b751588f4017b601127600f7cb463fd08355a
kazu1080601 <kazu1080601@gmail.com>
2020-12-18 09:53:16 +0900

    商品情報入力画面　販売価格以外実装

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,2 +1,14 @@
-class Area < ApplicationRecord
+class Area < ActiveHash::Base
+  require 'jp_prefecture'
+
+  field :name
+  create :id => 1, :name => '---'
+  47.times do |i|
+    pref = JpPrefecture::Prefecture.find i + 1
+    create :id => i + 2, :name => pref.name
+  end
+
+  include ActiveHash::Associations
+  has_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
   belongs_to :category
@@ -6,4 +8,6 @@ class Item < ApplicationRecord
   belongs_to :feebearing
   belongs_to :area
   belongs_to :preparation
+
+  validates :category_id, :condition_id, :feebearing_id, :area_id, :preparation_id, numericality: { other_than: 1 }
 end
