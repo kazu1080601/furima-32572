7318196f080807d095667aa044256eb6f5b8e4ad
kazu1080601 <kazu1080601@gmail.com>
2020-12-18 18:29:23 +0900

    binding.pry削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-    binding.pry
     if item.save
       redirect_to new_user_session_path
     else
