983b4d85bb93113e785083c4361fa04ec3cca695
kazu1080601 <kazu1080601@gmail.com>
2020-12-21 17:00:31 +0900

    商品編集機能　コントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user.id || params[:_method] == nil
+      redirect_to root_path
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
