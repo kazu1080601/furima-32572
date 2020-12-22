d05c0198d5941cec5644c5810eb494e0870d8999
kazu1080601 <kazu1080601@gmail.com>
2020-12-23 01:32:07 +0900

    ordersコントローラーのルーティングをitemsコントローラーとのネストに変更、購入画面のform実装（カード決済以外）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
   def index
-     @item = Item.find(params[:id])
+     @item = Item.find(params[:item_id])
+     @order = Order.new
+  end
+  
+  def create
+    binding.pry
   end
 end
