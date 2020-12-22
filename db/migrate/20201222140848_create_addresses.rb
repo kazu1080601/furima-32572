76cf3e440ebbe971c6637f0528e4a9b01fce008a
kazu1080601 <kazu1080601@gmail.com>
2020-12-22 23:20:24 +0900

    マイグレーション

--- db/migrate/20201222140848_create_addresses.rb
+++ db/migrate/20201222140848_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string      :post_number,    null: false
+      t.string      :prefecture,     null: false
+      t.string      :municipality,   null: false
+      t.string      :address,        null: false
+      t.string      :building
+      t.string      :phone,          null: false
+      t.references  :purchase,       foreign_key: true
       t.timestamps
     end
   end
