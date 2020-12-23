6bf49eeb165c5f2b94f5047b63abcdb0a96ab9c6
kazu1080601 <kazu1080601@gmail.com>
2020-12-23 12:32:06 +0900

    エラー対処のためAddressモデル再作成

--- db/migrate/20201222155046_remove_prefecture_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RemovePrefectureToAddresses < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :addresses, :prefecture, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_155359) do
+ActiveRecord::Schema.define(version: 2020_12_23_031356) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_155359) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_number", null: false
+    t.integer "area_id", null: false
     t.string "municipality", null: false
     t.string "address", null: false
     t.string "building"
@@ -42,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_12_22_155359) do
     t.bigint "purchase_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "area_id"
     t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
   end
 
